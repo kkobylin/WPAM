35de1a2b007fb2b5c30caa8c66a6edc76b64c74b
kkobylin <k.kobylinski98@gmail.com>
2020-04-22 10:00:01 +0200

    Button pressed

--- mobile_app/app/src/main/java/com/example/mobile_app/MainActivity.kt
+++ mobile_app/app/src/main/java/com/example/mobile_app/MainActivity.kt
@@ -1,112 +1,41 @@
 package com.example.mobile_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.MotionEvent
-import android.view.View
-import com.github.nkzawa.emitter.Emitter
-import com.github.nkzawa.socketio.client.IO
-import com.github.nkzawa.socketio.client.Socket
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.robot_steer_layout.*
-import java.net.URISyntaxException
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.welcome_screen.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    var moveBackwardEnable = true
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.robot_steer_layout)
-        val ipAddress = "192.168.2.109"
+        setContentView(R.layout.welcome_screen)
 
-        warnText.visibility = View.INVISIBLE
+        val ipAddressPattern =
+            ("^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$").toRegex()
 
-        //Variable must be initialized
-        var socket : Socket = IO.socket("http://localhost:3000")
-        try {
-            socket = IO.socket("http://$ipAddress:3000")
-            socket.connect()
-        }
-        catch (e : URISyntaxException) {
-            upperText.text = "Could not connect to the socket"
-            upperText.visibility = View.VISIBLE
-        }
+        nextButton.setOnClickListener { _ ->
+            val ip : String = ipEditText.text.toString()
+            if(ip.isNotEmpty()){
+                if (ipAddressPattern.matches(ip)) {
+                    val robotSteerIntent =
+                        Intent(applicationContext, RobotSteerActivity::class.java)
+                    robotSteerIntent.putExtra("ip", ip)
+                    startActivity(robotSteerIntent)
+                }
+                else{
+                    ipEditText.text.clear()
+                    ipEditText.hint = "Incorrect IP address"
+                }
 
-        downButton.setOnTouchListener { v, event ->
-            if(moveBackwardEnable) {
-                if (event.action == MotionEvent.ACTION_DOWN)
-                    socket.emit("move", "backward")
-                else if (event.action == MotionEvent.ACTION_UP)
-                    socket.emit("move", "stop")
             }
-            true
-        }
-
-        upButton.setOnTouchListener { v, event ->
-            if(event.action == MotionEvent.ACTION_DOWN)
-                socket.emit("move", "forward")
-            else if(event.action == MotionEvent.ACTION_UP)
-                socket.emit("move", "stop")
-            true
-        }
-
-        leftButton.setOnTouchListener { v, event ->
-            if(event.action == MotionEvent.ACTION_DOWN)
-                socket.emit("move", "left")
-            else if(event.action == MotionEvent.ACTION_UP)
-                socket.emit("move", "stop")
-            true
-        }
-
-        rightButton.setOnTouchListener { v, event ->
-            if(event.action == MotionEvent.ACTION_DOWN)
-                socket.emit("move", "right")
-            else if(event.action == MotionEvent.ACTION_UP)
-                socket.emit("move", "stop")
-            true
-        }
-
-        cameraUp.setOnTouchListener { v, event ->
-            if(event.action == MotionEvent.ACTION_DOWN)
-                socket.emit("camera", "up")
-            else if(event.action == MotionEvent.ACTION_UP)
-                socket.emit("camera", "stop")
-            true
-        }
-
-        cameraDown.setOnTouchListener { v, event ->
-            if(event.action == MotionEvent.ACTION_DOWN)
-                socket.emit("camera", "down")
-            else if(event.action == MotionEvent.ACTION_UP)
-                socket.emit("camera", "stop")
-            true
-        }
-
-        socket.on("connect") {
-            println("Connected to server")
-        }
-
-        socket.on("disconnect") {
-            println("Disconnected from server")
-        }
-
-        socket.on("obstacle", onObstacle)
-
-    }
-
-    private var onObstacle: Emitter.Listener = Emitter.Listener { args ->
-        runOnUiThread(Runnable {
-            val value = args[0] as Boolean
-            if(value){
-                warnText.text = "Obstacle alert"
-                warnText.visibility = View.VISIBLE
-                moveBackwardEnable = false
+            else{
+                ipEditText.hint = "You must enter IP address";
             }
-            else {
-                warnText.visibility = View.INVISIBLE
-                moveBackwardEnable = true
-            }
-        })
+        }
     }
 }
---
+++ mobile_app/app/src/main/java/com/example/mobile_app/RobotSteerActivity.kt
@@ -0,0 +1,145 @@
+package com.example.mobile_app
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobile_app.R.drawable.button_background
+import com.github.nkzawa.emitter.Emitter
+import com.github.nkzawa.socketio.client.IO
+import com.github.nkzawa.socketio.client.Socket
+import kotlinx.android.synthetic.main.robot_steer_one.*
+import java.net.URISyntaxException
+
+
+class RobotSteerActivity : AppCompatActivity() {
+
+    var moveBackwardEnable = true
+
+    @SuppressLint("SourceLockedOrientationActivity")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.robot_steer_one)
+
+        /* 192.168.88.52 */
+        val ipAddress: String = if(intent != null) {
+            if (intent.hasExtra("ip"))
+                intent.extras?.getString("ip").toString()
+            else
+                "No extra"
+        } else
+            "No extra"
+
+        warnText.visibility = View.INVISIBLE
+
+        webView.loadUrl("http://$ipAddress:3080")
+
+        //Variable must be initialized
+        var socket : Socket = IO.socket("http://localhost:3000")
+        try {
+            socket = IO.socket("http://$ipAddress:3000")
+            socket.connect()
+        }
+        catch (e : URISyntaxException) {
+            warnText.text = "Could not connect to the socket"
+            warnText.visibility = View.VISIBLE
+        }
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+        hideSystemUI()
+
+        downButton.setOnTouchListener { v, event ->
+            if(moveBackwardEnable) {
+                if (event.action == MotionEvent.ACTION_DOWN)
+                    socket.emit("move", "backward")
+                else if (event.action == MotionEvent.ACTION_UP)
+                    socket.emit("move", "stop")
+            }
+            true
+        }
+
+        upButton.setOnTouchListener { v, event ->
+            if(event.action == MotionEvent.ACTION_DOWN)
+                socket.emit("move", "forward")
+            else if(event.action == MotionEvent.ACTION_UP)
+                socket.emit("move", "stop")
+            true
+        }
+
+        leftButton.setOnTouchListener { v, event ->
+            if(event.action == MotionEvent.ACTION_DOWN)
+                socket.emit("move", "left")
+            else if(event.action == MotionEvent.ACTION_UP)
+                socket.emit("move", "stop")
+            true
+        }
+
+        rightButton.setOnTouchListener { v, event ->
+            if(event.action == MotionEvent.ACTION_DOWN)
+                socket.emit("move", "right")
+            else if(event.action == MotionEvent.ACTION_UP)
+                socket.emit("move", "stop")
+            true
+        }
+
+        cameraUp.setOnTouchListener { v, event ->
+            if(event.action == MotionEvent.ACTION_DOWN)
+                socket.emit("camera", "up")
+            else if(event.action == MotionEvent.ACTION_UP)
+                socket.emit("camera", "stop")
+            true
+        }
+
+        cameraDown.setOnTouchListener { v, event ->
+            if(event.action == MotionEvent.ACTION_DOWN)
+                socket.emit("camera", "down")
+            else if(event.action == MotionEvent.ACTION_UP)
+                socket.emit("camera", "stop")
+            true
+        }
+
+        socket.on("connect") {
+            println("Connected to server")
+        }
+
+        socket.on("disconnect") {
+            println("Disconnected from server")
+        }
+
+        socket.on("obstacle", onObstacle)
+
+        //todo send back settings
+        backButton.setOnClickListener { _ ->
+            val mainIntent = Intent(applicationContext, MainActivity::class.java )
+            startActivity(mainIntent)
+        }
+
+    }
+
+    private var onObstacle: Emitter.Listener = Emitter.Listener { args ->
+        runOnUiThread(Runnable {
+            val value = args[0] as Boolean
+            if(value){
+                warnText.text = "Obstacle alert"
+                warnText.visibility = View.VISIBLE
+                moveBackwardEnable = false
+            }
+            else {
+                warnText.visibility = View.INVISIBLE
+                moveBackwardEnable = true
+            }
+        })
+    }
+
+    fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
+}
